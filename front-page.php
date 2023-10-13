2ff10dea3573d8dd6d5f5ac65cb49f07a5d9bd9b
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-13 20:41:08 +0300

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col-reverse md:flex-row md:max-lg:mt-14 mb-14 lg:mb-0">
   <div class="flex-1 self-center text-center md:text-left">
     <h1 class="text-3xl md:text-5xl font-extrabold mb-1 md:mb-4">KOMPIUTERIŲ SERVISAS VILNIUJE</h1>
-    <p class="text-lg md:text-2xl my-2 md:my-5">Greitai nustatysime gedimą bei rasime sprendimą. Taisome, tobuliname bei surenkame nešiojamus ir stacionarius kompiuterius.</p>
+    <p class="text-lg md:text-2xl my-2 md:my-5">Greitai nustatysime gedimą bei rasime sprendimą. Taisome, tobuliname bei surenkame kompiuterius. Kreipkitės jau šiandien, visada padėsime!</p>
     <a href="<?php echo get_page_link(12); ?>" class="btn btn--red rounded-lg inline-block text-lg md:text-xl px-12 py-4 mt-2 md:mt-0">PASLAUGŲ KAINOS</a>
   </div>
   <div class="flex-1">
