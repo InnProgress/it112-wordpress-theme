bce1d485a46189fa1896e3226752aa5dbce1bdec
Arnas Bulka <40238781+Arnanasas@users.noreply.github.com>
2023-10-17 00:34:10 +0300

    Update page-apie-mus.php

--- page-apie-mus.php
+++ page-apie-mus.php
@@ -4,7 +4,7 @@
   <div class="lg:w-5/12 self-center text-center lg:text-left">
     <h1 class="text-3xl md:text-5xl font-bold uppercase mb-1 md:mb-4">Apie mus</h1>
     <p class="text-lg md:text-2xl my-2 md:my-5">Daug daug teksto orientuoto į raktažodžius kompiuterių remontas vilniuje, kompiuterių taisykla ir t.t. Useriui jis nesvarbus - svarbus paieškos sistemoms.
-    <a href="<?php echo get_page_link(12); ?>" class="btn btn--red rounded-lg inline-block text-lg md:text-xl px-12 py-4 mt-2 md:mt-0">PASLAUGŲ KAINOS</a>
+    <a href="<?php echo get_page_link(12); ?>" class="btn btn--red rounded-lg inline-block text-lg md:text-xl px-12 py-4 mt-2 my-2 md:mt-0">PASLAUGŲ KAINOS</a>
   </div>
   <div class="flex-1">
     <img src="<?php echo get_template_directory_uri(); ?>/img/puzzle.png" class="mx-auto lg:ml-auto lg:mr-0 w-10/12">
