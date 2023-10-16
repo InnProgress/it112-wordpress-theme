243f254bd6ebaac50f8b45f36554fca08a15989e
Arnanasas <arnas.bulk@gmail.com>
2023-10-17 00:18:04 +0300

    kontaktai

--- page-kontaktai.php
+++ page-kontaktai.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col-reverse lg:flex-row mb-14 md:mb-0">
   <div class="lg:w-5/12 self-center text-center lg:text-left">
     <h1 class="text-3xl md:text-5xl font-bold uppercase mb-1 md:mb-4">Susisiekime!</h1>
-    <p class="text-lg md:text-2xl my-2 md:my-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione eveniet cumque mollitia qui ut enim corporis dignissimos itaque velit, ipsam nobis a illum quidem obcaecati? Iure quia unde quo doloremque? <a href="mailto:info@it112.lt>info@it112.lt</a> <a href="tel:+37062249444">+3705...</a> </p>
+    <p class="text-lg md:text-2xl my-2 md:my-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione eveniet cumque mollitia qui ut enim corporis dignissimos itaque velit, ipsam nobis a illum quidem obcaecati? Iure quia unde quo doloremque? <a href="mailto:info@it112.lt" class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">info@it112.lt </a> <a href="tel:+37062249444" class="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">+3705...</a> </p>
   </div>
   <div class="flex-1">
     <img src="<?php echo get_template_directory_uri(); ?>/img/puzzle.png" class="mx-auto lg:ml-auto lg:mr-0 w-10/12">
@@ -15,7 +15,7 @@
     <img src="<?php echo get_template_directory_uri(); ?>/img/new-entries.png" class="mx-auto lg:mr-auto lg:ml-0 w-10/12 lg:w-full">
   </div>  
   <div class="lg:w-6/12 self-center">
-    <h2 class="text-2xl md:text-4xl font-bold uppercase text-center lg:text-left mb-1 md:mb-4">Pradėkime jau dabar!</h2>
+    <h2 class="text-2xl md:text-4xl font-bold uppercase text-center lg:text-left mb-1 md:mb-4">Kreipkitės, o mes pažadame atsakyti kaip įmanoma greičiau!</h2>
     <?php echo do_shortcode('[contact-form-7 id="47162d4" title="Kontaktų forma"]'); ?>
     <!-- DO NOT REMOVE THESE COMMENTS AS THEY ARE HERE TO MAKE SURE TAILWIND FINDS CLASSES USED IN CONTACT FORM 7 FORMS TO COMPILE THEM TO CSS, OTHERWISE THEY WILL NOT BE INCLUDED IN FINAL CSS FILE -->
     <!-- mt-1 mt-6 cursor-pointer -->
