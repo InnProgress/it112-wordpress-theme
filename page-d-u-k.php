6a3b7a548ff719f94e15f28ad5948fa4983fbc78
Arnanasas <arnas.bulk@gmail.com>
2023-10-17 00:01:15 +0300

    faq fix

--- page-d-u-k.php
+++ page-d-u-k.php
@@ -1,14 +1,6 @@
 <?php get_header(); ?>
 
 <div class="flex flex-col-reverse lg:flex-row justify-between mb-14 md:mb-0">
-  <div class="lg:w-6/12 self-center text-center lg:text-left lg:pr-7">
-    <h1 class="text-3xl lg:text-4xl xl:text-5xl font-extrabold uppercase mb-1 md:mb-4">Neįsijungia? Lėtas?<br />O gal sulūžo?</h1>
-    <p class="text-lg lg:text-xl xl:text-2xl my-2 md:my-5">Nepriklausomai nuo kompiuterio modelio ar gedimo, mes visada pasirengę suteikti aukščiausios kokybės paslaugas.</p>
-    <p class="text-lg lg:text-xl xl:text-2xl my-2 md:my-5">Visiems darbams suteikiama <strong>3 mėnesių garantija!</strong></p>
-  </div>
-  <div class="flex-1">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/maintenance.png" class="mx-auto w-9/12 lg:w-full">
-  </div>
   <div class="container mx-auto py-12 px-4">
     <h1 class="text-3xl font-bold mb-10 text-center">Frequently Asked Questions</h1>
     
