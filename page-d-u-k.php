344b99d14c6b84f0f4c94e84e8e1c53f8b67e482
Arnanasas <arnas.bulk@gmail.com>
2023-10-16 23:57:39 +0300

    faq

--- page-d-u-k.php
+++ page-d-u-k.php
@@ -9,160 +9,32 @@
   <div class="flex-1">
     <img src="<?php echo get_template_directory_uri(); ?>/img/maintenance.png" class="mx-auto w-9/12 lg:w-full">
   </div>
+  <div class="container mx-auto py-12 px-4">
+    <h1 class="text-3xl font-bold mb-10 text-center">Frequently Asked Questions</h1>
+    
+    <div class="max-w-3xl mx-auto">
+        <div class="border rounded-md shadow-md bg-white mb-6">
+            <div class="px-5 py-3 border-b">
+                <h2 class="text-xl font-medium">What is the purpose of this FAQ page?</h2>
+            </div>
+            <div class="px-5 py-3">
+                This FAQ page provides answers to common questions about our services and platform.
+            </div>
+        </div>
+        
+        <div class="border rounded-md shadow-md bg-white mb-6">
+            <div class="px-5 py-3 border-b">
+                <h2 class="text-xl font-medium">How do I use this service?</h2>
+            </div>
+            <div class="px-5 py-3">
+                To use our service, simply sign up and follow the on-screen instructions.
+            </div>
+        </div>
+
+    </div>
+</div>
 </div>
 
-<div class="flex flex-col md:flex-row md:space-x-3 space-y-6 md:space-y-0">
-    <div class="flex-1">
-        <div class="btn btn--blue btn--no-hover rounded-md text-center text-xl uppercase p-3">Stacionarūs kompiuteriai</div>
-        <div class="border border-solid border-slate-300 rounded-lg mt-5">
-            <table class="border-collapse w-full text-left">
-                <thead>
-                    <tr>
-                        <th class="border-b border-slate-300 p-4 pl-9">Darbas</th>
-                        <th class="border-b border-slate-300 p-4 pr-9 text-center">Kaina</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Kompiuterio detalių paieška ir surinkimo darbai</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">50€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Diagnostika</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">15€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Windows įrašymas be duomenų perkėlimo</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">25€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Windows įrašymas su duomenų perkėlimu</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Duomenų perkėlimas/saugojimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Aušinimo profilaktika</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Procesoriaus įdėjimas/keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Vaizdo plokštės įdėjimas/keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">15€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Pagrindinės plokštės keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Maitinimo bloko keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">25€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">RAM įdėjimas/keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">15€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">SSD įdėjimas/keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€</td>
-                    </tr>
-                    <tr>
-                        <td class="p-4 pl-9">Korpuso keitimas</td>
-                        <td class="p-4 pr-9 text-center">35€</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <div class="flex-1">
-        <div class="btn btn--yellow btn--no-hover rounded-md text-center text-xl uppercase p-3">Nešiojami kompiuteriai</div>
-        <div class="border border-solid border-slate-300 rounded-lg mt-5">
-            <table class="border-collapse w-full text-left">
-                <thead>
-                    <tr>
-                        <th class="border-b border-slate-300 p-4 pl-9">Darbas</th>
-                        <th class="border-b border-slate-300 p-4 pr-9 text-center">Kaina</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Diagnostika, kai nėra ardomas kompiuteris</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">0€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Diagnostika, kai yra ardomas kompiuteris</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">15€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Windows įrašymas be duomenų perkėlimo</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">25€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Windows įrašymas su duomenų perkėlimu</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Duomenų perkėlimas/saugojimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Aušinimo profilaktika</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">25€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Sulūžusių korpusų restauravimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">50€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Skysčiais užlietų kompiuterių valymas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Krovimo lizdo keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">40€</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Klaviatūros keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">30€ + detalė</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Korpuso detalės keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€ + detalė</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Aušintuvo keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€ + detalė</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Ekrano keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€ + detalė</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">Baterijos keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€ + detalė</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">HDD, SSD disko įdėjimas/keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€ + detalė</td>
-                    </tr>
-                    <tr>
-                        <td class="border-b border-slate-300 p-4 pl-9">RAM įdėjimas/keitimas</td>
-                        <td class="border-b border-slate-300 p-4 pr-9 text-center">20€ + detalė</td>
-                    </tr>
-                    <tr>
-                        <td class="p-4 pl-9">Pagrindinės plokštės remontas</td>
-                        <td class="p-4 pr-9 text-center">Nuo 120€</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
 
 <?php require 'dirbame-su.php'; ?>
 
