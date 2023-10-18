e4c4759e95e6ef1a8c8d73783b7711b2f85518e1
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-18 10:54:23 +0300

    Update page-apie-mus.php

--- page-apie-mus.php
+++ page-apie-mus.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<div class="flex flex-col-reverse lg:flex-row mb-14 md:mb-0">
-  <div class="lg:w-5/12 self-center text-center lg:text-left">
-    <h1 class="text-3xl md:text-5xl font-bold uppercase mb-1 md:mb-4">Apie mus</h1>
-    <p class="text-lg md:text-2xl my-2 md:my-5">Daug daug teksto orientuoto į raktažodžius kompiuterių remontas vilniuje, kompiuterių taisykla ir t.t. Useriui jis nesvarbus - svarbus paieškos sistemoms.
-    <a href="<?php echo get_page_link(12); ?>" class="btn btn--red rounded-lg inline-block text-lg md:text-xl px-12 py-4 mt-2 my-2 md:mt-0">PASLAUGŲ KAINOS</a>
+<div class="flex flex-col-reverse md:flex-row md:max-lg:mt-14 mb-14 lg:mb-0">
+  <div class="flex-1 self-center text-center md:text-left">
+    <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold mb-1 md:mb-4">KOMPIUTERIŲ SERVISAS VILNIUJE</h1>
+    <p class="text-lg md:text-xl lg:text-2xl my-2 md:my-5">Greitai nustatysime gedimą bei rasime sprendimą. Taisome, tobuliname bei surenkame kompiuterius. Kreipkitės jau šiandien, visada padėsime!</p>
+    <a href="<?php echo get_page_link(12); ?>" class="btn btn--red rounded-lg inline-block text-lg md:text-xl px-12 py-4 mt-2 md:mt-0">PASLAUGŲ KAINOS</a>
   </div>
   <div class="flex-1">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/puzzle.png" class="mx-auto lg:ml-auto lg:mr-0 w-10/12">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/troubleshooting.png" class="mx-auto w-9/12 md:w-full">
   </div>
 </div>
 
