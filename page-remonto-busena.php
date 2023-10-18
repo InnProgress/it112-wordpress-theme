9c03257e6a3ab009854cd571dcecfcb4a5089048
Arnold Autuch <arnoldautuch4@gmail.com>
2023-10-18 14:28:18 +0300

    change device status page to have multiple status updates for device

--- page-remonto-busena.php
+++ page-remonto-busena.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="flex flex-col-reverse lg:flex-row mb-14 md:mb-0">
-  <div class="status-check-container self-center w-full lg:w-6/12 text-center lg:text-left">
+  <div class="status-check-container self-center w-full lg:w-6/12 text-center lg:text-left pt-8">
     <h2 class="text-2xl md:text-4xl font-bold uppercase mb-2 md:mb-4">Pasitikrink savo įrenginio remonto eigą</h2>
     <p class="text-md md:text-xl my-2 md:my-5">Remonto numeris yra nurodytas kompiuterio priėmimo akte</p>
     <form class="status-check-form">
@@ -9,7 +9,7 @@
       <button class="btn btn--yellow rounded-lg text-md md:text-xl px-14 py-5">TIKRINTI</a>
     </form>
   </div>
-  <div class="flex flex-1 justify-end">
+  <div class="flex flex-1 justify-end items-center">
     <img src="<?php echo get_template_directory_uri(); ?>/img/remote-working.png" class="mx-auto md:mx-0 w-11/12">
   </div>
 </div>
--- script.js
+++ script.js
@@ -23,9 +23,17 @@ window.onload = () => {
       const data = {
         model: "Dell Latitude 3510",
         price: "120€",
-        status:
-          "Laukiame naujo aušintuvo. Apie 2-3 savaites. Test test test test test test.",
-        statusUpdate: "2023-08-26 11:05",
+        statusUpdates: [
+          {
+            status:
+              "Laukiame naujo aušintuvo. Apie 2-3 savaites. Test test test test test test.",
+            date: "2023-08-26 11:05",
+          },
+          {
+            status: "Testing",
+            date: "2023-09-01 12:01",
+          },
+        ],
       };
 
       const container = document.querySelector(".status-check-container");
@@ -33,23 +41,35 @@ window.onload = () => {
         <div>
           <div class="mb-2">
             <label class="block text-xl font-bold mb-2" for="model-name">Kompiuterio modelis</label>
-            <input value="${data.model}" name="model-name" class="block w-full text-xl rounded-lg border border-solid border-gray-200 focus:outline-yellow-400 bg-transparent hover:cursor-default px-3 md:px-5 py-3 md:py-5 mb-3 md:mb-5" readonly />
+            <input value="${
+              data.model
+            }" name="model-name" class="block w-full text-xl rounded-lg border border-solid border-gray-200 focus:outline-yellow-400 bg-transparent hover:cursor-default px-3 md:px-5 py-3 md:py-5 mb-3 md:mb-5" readonly />
           </div>
           <div class="mb-2">
             <label class="block text-xl font-bold mb-2" for="price">Remonto kaina</label>
-            <input value="${data.price}" name="price" class="block w-full text-xl rounded-lg border border-solid border-gray-200 focus:outline-yellow-400 bg-transparent hover:cursor-default px-3 md:px-5 py-3 md:py-5 mb-3 md:mb-5" readonly />
+            <input value="${
+              data.price
+            }" name="price" class="block w-full text-xl rounded-lg border border-solid border-gray-200 focus:outline-yellow-400 bg-transparent hover:cursor-default px-3 md:px-5 py-3 md:py-5 mb-3 md:mb-5" readonly />
           </div>
           <div>
             <div class="flex justify-between mb-2">
               <label class="text-xl font-bold" for="status">Būsena</label>
-              <p class="text-xl text-gray-400">${data.statusUpdate}</p>
             </div>
-            <textarea
-              name="status"
-              class="resize-none block w-full text-xl rounded-lg border border-solid border-gray-200 focus:outline-yellow-400 bg-transparent hover:cursor-default px-3 md:px-5 py-3 md:py-5 mb-3 md:mb-5"
-              rows="5"
-              readonly
-            >${data.status}</textarea>
+            ${data.statusUpdates
+              .map(
+                (statusUpdate) => `
+              <div class="flex justify-between mb-2">
+                <p class="text-xl text-gray-400">${statusUpdate.date}</p>
+              </div>
+              <textarea
+                name="status"
+                class="resize-none block w-full text-xl rounded-lg border border-solid border-gray-200 focus:outline-yellow-400 bg-transparent hover:cursor-default px-3 md:px-5 py-3 md:py-5 mb-3 md:mb-5"
+                rows="3"
+                readonly
+              >${statusUpdate.status}</textarea>
+            `
+              )
+              .join("")}
           </div>
         </div>
       `;
