f7d874dffc208240b2464d6d15b3b965f3058282
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-18 12:19:38 +0300

    Update dirbame-su.php

--- dirbame-su.php
+++ dirbame-su.php
@@ -1,12 +1,12 @@
 <div class="text-center my-14">
   <h2 class="text-2xl md:text-4xl font-extrabold mb-6 uppercase">Dirbame su</h2>
   <div class="working-with-container flex flex-col md:flex-row space-y-14 md:space-y-0 md:space-x-14 justify-center items-center bg-slate-100 rounded-lg px-4 py-9">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/dell-logo.png" width="100" height="auto"/>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/lenovo-logo.png" width="100" height="auto"/>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/asus-logo.png" width="130" height="auto"/>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/hp-logo.png" width="100" height="auto"/>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/acer-logo.png" width="100" height="auto"/>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/dell-logo.png" width="100" height="auto"/>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/samsung-logo.png" width="100" height="auto"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/dell-logo.png" width="auto" height="100"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/lenovo-logo.png" width="auto" height="65"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/asus-logo.png" width="auto" height="65"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/hp-logo.png" width="auto" height="100"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/acer-logo.png" width="auto" height="65"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/dell-logo.png" width="auto" height="100"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/samsung-logo.png" width="auto" height="65"/>
   </div>
 </div>
