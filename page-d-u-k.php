bb5d91b547a46c2f176e9eced2a839f934f22f8e
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-18 10:30:52 +0300

    Update page-d-u-k.php

--- page-d-u-k.php
+++ page-d-u-k.php
@@ -2,9 +2,9 @@
 
 <div class="flex flex-col-reverse lg:flex-row justify-between mb-14 md:mb-0">
   <div class="container mx-auto py-12 px-4">
-    <div class="max-w-3xl mx-auto btn btn--yellow btn--no-hover rounded-md text-center text-xl uppercase p-3 mb-3">Dažniausiai užduodami klausimai</div>
+    <div class="btn btn--yellow btn--no-hover rounded-md text-center text-xl uppercase p-3 mb-3">Dažniausiai užduodami klausimai</div>
     
-    <div class="max-w-3xl mx-auto">
+    <div>
         <div class="border rounded-md shadow-md bg-white mb-6">
             <div class="px-5 py-3 border-b">
                 <h2 class="text-xl font-bold">What is the purpose of this FAQ page?</h2>
