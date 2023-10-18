9987832fc3321d278560803dd4930380e0264b3c
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-18 10:37:41 +0300

    Update page-d-u-k.php

--- page-d-u-k.php
+++ page-d-u-k.php
@@ -5,12 +5,40 @@
     <div class="btn btn--blue btn--no-hover rounded-md text-center text-xl uppercase p-3 mb-3">Dažniausiai užduodami klausimai</div>
     
     <div>
+      
         <div class="border rounded-md bg-white mb-6">
             <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-bold">What is the purpose of this FAQ page?</h2>
+                <h2 class="text-xl font-bold">Kokiu adresu mus galite rasti?</h2>
             </div>
             <div class="px-5 py-3">
-                This FAQ page provides answers to common questions about our services and platform.
+                Adresas.
+            </div>
+        </div>
+
+        <div class="border rounded-md bg-white mb-6">
+            <div class="px-5 py-3 border-b">
+                <h2 class="text-xl font-bold">Koks mūsų darbo laikas?</h2>
+            </div>
+            <div class="px-5 py-3">
+                Mes dirbame I-V, 10-18 val. VI, 10-15 val.
+            </div>
+        </div>
+
+          <div class="border rounded-md bg-white mb-6">
+            <div class="px-5 py-3 border-b">
+                <h2 class="text-xl font-bold">Per kiek laiko atliekame kompiuteriui diagnostiką?</h2>
+            </div>
+            <div class="px-5 py-3">
+                Kiekviena situacija yra individuali. Vidutinškai gedimą nustatome greičiau nei per valandą.
+            </div>
+        </div>
+
+            <div class="border rounded-md bg-white mb-6">
+            <div class="px-5 py-3 border-b">
+                <h2 class="text-xl font-bold">Kiek laiko užtrunka aušinimo valymas, termopastos keitimas?</h2>
+            </div>
+            <div class="px-5 py-3">
+                Dažniausiai užtrunkame apie 20-30 minučių.
             </div>
         </div>
 
