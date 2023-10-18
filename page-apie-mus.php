573648d39f8f4d971bd08246bddc78ed67e561ef
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-18 11:16:26 +0300

    Update page-apie-mus.php

--- page-apie-mus.php
+++ page-apie-mus.php
@@ -2,9 +2,9 @@
 
 <div class="flex flex-col-reverse md:flex-row md:max-lg:mt-14 mb-14 lg:mb-0">
   <div class="flex-1 self-center text-center md:text-left">
-    <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold mb-1 md:mb-4">KOMPIUTERIŲ SERVISAS VILNIUJE</h1>
-    <p class="text-lg md:text-xl lg:text-2xl my-2 md:my-5">Greitai nustatysime gedimą bei rasime sprendimą. Taisome, tobuliname bei surenkame kompiuterius. Kreipkitės jau šiandien, visada padėsime!</p>
-    <a href="<?php echo get_page_link(12); ?>" class="btn btn--red rounded-lg inline-block text-lg md:text-xl px-12 py-4 mt-2 md:mt-0">PASLAUGŲ KAINOS</a>
+    <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold mb-1 md:mb-4">TRUMPAI APIE MUS</h1>
+    <p class="text-lg md:text-xl lg:text-2xl my-2 md:my-5">Esame motyvuota nešiojamų ir stacionarių kompiuterių taisykla. Didelę patirtį sukaupę specialistai pasirengę suteikti aukščiausios kokybės paslaugas. Jūsų pasitikėjimas yra mūsų prioritetas, todėl dirbame, kad užtikrintume jūsų techninės įrangos sklandų veikimą.</p>
+    <a href="<?php echo get_page_link(12); ?>" class="btn btn--red rounded-lg inline-block text-lg md:text-xl px-12 py-4 mt-2 md:mt-0">PASLAUGOS</a>
   </div>
   <div class="flex-1">
     <img src="<?php echo get_template_directory_uri(); ?>/img/troubleshooting.png" class="mx-auto w-9/12 md:w-full">
