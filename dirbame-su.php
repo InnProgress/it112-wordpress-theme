7123e4c49173230f20693ab122176bbca5fa723e
Arnold Autuch <arnoldautuch4@gmail.com>
2023-10-22 15:46:33 +0300

    hide dirbame su for mobile phones

--- dirbame-su.php
+++ dirbame-su.php
@@ -1,4 +1,4 @@
-<div class="text-center my-14">
+<div class="hidden md:block text-center my-14">
   <h2 class="text-2xl md:text-4xl font-extrabold mb-6 uppercase">Dirbame su</h2>
   <div class="working-with-container flex flex-col flex-wrap md:flex-row md:space-x-14 justify-center items-center bg-slate-100 rounded-lg px-4 pt-9 pb-5">
     <img src="<?php echo get_template_directory_uri(); ?>/img/dell-logo.png" width="80" height="95" class="mb-8 md:mb-4"/>
