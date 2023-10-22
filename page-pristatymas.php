a7b1cabffb9193bfd2e943c71bc7aeb23259ac90
Arnold Autuch <arnoldautuch4@gmail.com>
2023-10-22 16:11:24 +0300

    fix text align for pristatymas

--- page-pristatymas.php
+++ page-pristatymas.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col-reverse lg:flex-row mt-6 mb-14 md:mb-12">
   <div class="lg:w-5/12 self-center text-center lg:text-left">
     <h1 class="text-3xl md:text-5xl font-bold uppercase mb-1 md:mb-4">Pristatykite kurjeriu ar paštomatu!</h1>
-    <p class="text-lg md:text-2xl my-2 md:my-5">Jei neturite laiko atvykti patys ar gyvenate toli nuo mūsų salono, pasinaudokite kurjerių bei paštomatų pagalba. Sutaisysime įrenginį, saugiai įpakuosime ir išsiųsime Jums nemokamai!</p>
+    <p class="text-lg md:text-2xl text-justify lg:text-left my-2 md:my-5">Jei neturite laiko atvykti patys ar gyvenate toli nuo mūsų salono, pasinaudokite kurjerių bei paštomatų pagalba. Sutaisysime įrenginį, saugiai įpakuosime ir išsiųsime Jums nemokamai!</p>
   </div>
   <div class="flex-1 mb-6 lg:mb-0">
     <img src="<?php echo get_template_directory_uri(); ?>/img/delivery-guy.png" class="mx-auto lg:ml-auto lg:mr-0 w-10/12">
