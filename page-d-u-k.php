5365213f054827537c752dd57836116c05135755
Arnold Autuch <arnoldautuch4@gmail.com>
2023-10-22 12:38:38 +0300

    update design of faq

--- page-d-u-k.php
+++ page-d-u-k.php
@@ -19,74 +19,72 @@
     </div>
     
     <div>
+      <div class="border border-slate-300 rounded-md bg-white mb-6">
+        <div class="bg-gray-100	border-b border-slate-300 rounded-t-md px-5 py-3">
+          <h2 class="text-xl font-medium">Kokiu adresu mus galite rasti?</h2>
+        </div>
+        <div class="px-5 py-3">
+          Adresas.
+        </div>
+      </div>
       
-        <div class="border rounded-md bg-white mb-6">
-            <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-medium">Kokiu adresu mus galite rasti?</h2>
-            </div>
-            <div class="px-5 py-3">
-                Adresas.
-            </div>
+      <div class="border border-slate-300 rounded-md bg-white mb-6">
+        <div class="bg-gray-100	border-b border-slate-300 rounded-t-md px-5 py-3">
+          <h2 class="text-xl font-medium">Koks mūsų darbo laikas?</h2>
         </div>
+        <div class="px-5 py-3">
+          Mes dirbame I-V, 10-18 val. VI, 10-15 val.
+        </div>
+      </div>
 
-        <div class="border rounded-md bg-white mb-6">
-            <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-medium">Koks mūsų darbo laikas?</h2>
-            </div>
-            <div class="px-5 py-3">
-                Mes dirbame I-V, 10-18 val. VI, 10-15 val.
-            </div>
+      <div class="border border-slate-300 rounded-md bg-white mb-6">
+        <div class="bg-gray-100 border-b border-slate-300 rounded-t-md px-5 py-3">
+          <h2 class="text-xl font-medium">Per kiek laiko atliekame kompiuteriui diagnostiką?</h2>
         </div>
+        <div class="px-5 py-3">
+          Kiekviena situacija yra individuali. Vidutinškai gedimą nustatome greičiau nei per valandą.
+        </div>
+      </div>
 
-          <div class="border rounded-md bg-white mb-6">
-            <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-medium">Per kiek laiko atliekame kompiuteriui diagnostiką?</h2>
-            </div>
-            <div class="px-5 py-3">
-                Kiekviena situacija yra individuali. Vidutinškai gedimą nustatome greičiau nei per valandą.
-            </div>
+      <div class="border border-slate-300 rounded-md bg-white mb-6">
+        <div class="bg-gray-100 border-b border-slate-300 rounded-t-md px-5 py-3">
+          <h2 class="text-xl font-medium">Kiek laiko užtrunka aušinimo valymas, termopastos keitimas?</h2>
         </div>
+        <div class="px-5 py-3">
+          Dažniausiai užtrunkame apie 20-30 minučių.
+        </div>
+      </div>
 
-          <div class="border rounded-md bg-white mb-6">
-            <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-medium">Kiek laiko užtrunka aušinimo valymas, termopastos keitimas?</h2>
-            </div>
-            <div class="px-5 py-3">
-                Dažniausiai užtrunkame apie 20-30 minučių.
-            </div>
+      <div class="border border-slate-300 rounded-md bg-white mb-6">
+        <div class="bg-gray-100 border-b border-slate-300 rounded-t-md px-5 py-3">
+          <h2 class="text-xl font-medium">Kiek laiko užtrunka Windows perrašymo darbai?</h2>
         </div>
-
-          <div class="border rounded-md bg-white mb-6">
-            <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-medium">Kiek laiko užtrunka Windows perrašymo darbai?</h2>
-            </div>
-            <div class="px-5 py-3">
-                Jei prieš Windows perrašymą nereikia išsaugoti jokių duomenų, tada užtrunkama apie 1-2 valandas. Tačiau jei duomenis reikia išsaugoti - užtrunkama ilgiau, o laikas priklauso nuo duomenų kiekio.
-            </div>
+        <div class="px-5 py-3">
+          Jei prieš Windows perrašymą nereikia išsaugoti jokių duomenų, tada užtrunkama apie 1-2 valandas. Tačiau jei duomenis reikia išsaugoti - užtrunkama ilgiau, o laikas priklauso nuo duomenų kiekio.
         </div>
+      </div>
       
-          <div class="border rounded-md bg-white mb-6">
-            <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-medium">Ar atliktiems darbams suteikiame garantiją?</h2>
-            </div>
-            <div class="px-5 py-3">
-                Visiems remonto darbams bei serviso dalims suteikiame 3 mėnesių garantiją.
-            </div>
+      <div class="border border-slate-300 rounded-md bg-white mb-6">
+        <div class="bg-gray-100 border-b border-slate-300 rounded-t-md px-5 py-3">
+          <h2 class="text-xl font-medium">Ar atliktiems darbams suteikiame garantiją?</h2>
         </div>
+        <div class="px-5 py-3">
+          Visiems remonto darbams bei serviso dalims suteikiame 3 mėnesių garantiją.
+        </div>
+      </div>
 
-          <div class="border rounded-md bg-white mb-6">
-            <div class="px-5 py-3 border-b">
-                <h2 class="text-xl font-medium">Ar kompiuterio remonto metu bus pažeisti/prarasti duomenys?</h2>
-            </div>
-            <div class="px-5 py-3">
-                Ne. Jei yra rizika, kad duomenys gali būti prarasti, mes klientą visada įspėjame. Jūsų duomenų saugumas yra mūsų prioritetas.
-            </div>
+      <div class="border border-slate-300 rounded-md bg-white mb-6">
+        <div class="bg-gray-100 border-b border-slate-300 rounded-t-md px-5 py-3">
+          <h2 class="text-xl font-medium">Ar kompiuterio remonto metu bus pažeisti/prarasti duomenys?</h2>
         </div>
+        <div class="px-5 py-3">
+          Ne. Jei yra rizika, kad duomenys gali būti prarasti, mes klientą visada įspėjame. Jūsų duomenų saugumas yra mūsų prioritetas.
+        </div>
+      </div>
 
     </div>
+  </div>
 </div>
-</div>
-
 
 <?php require 'dirbame-su.php'; ?>
 
