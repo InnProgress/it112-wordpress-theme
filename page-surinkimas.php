5ea554bac9631b3ced77f2bea88c1351d4c71e4c
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-25 08:57:40 +0300

    Update page-surinkimas.php

--- page-surinkimas.php
+++ page-surinkimas.php
@@ -6,7 +6,7 @@
     <p class="text-lg md:text-2xl text-justify lg:text-left my-2 md:my-5">Galime surinkti itin galingus, žaidimams ir sudėtingam darbui pritaikytus arba biudžetinius, mokslams ir įprastam laisvalaikiui skirtus stacionarius kompiuterius. Parinksime tik kokybiškus komponentus. Suteiksime garantiją.</p>
   </div>
   <div class="flex-1 mb-3 lg:mb-0">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/puzzle.png" class="mx-auto lg:ml-auto lg:mr-0 w-10/12">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/puzzle.png" class="mx-auto lg:ml-auto lg:mr-0 w-10/12" alt="Lėtas kompiuteris">
   </div>
 </div>
 
@@ -15,7 +15,7 @@
     <div class="info-block block text-center rounded-xl border-solid border-2 border-gray-300 px-3 py-14 w-full">
       <div class="info-block__inside">
        <div class="bg-blue rounded-full inline-flex w-[131px] h-[131px] text-center p-4 mb-6">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/chat-icon.png" width="59" height="62" class="self-center mx-auto">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/chat-icon.png" width="59" height="62" class="self-center mx-auto" alt="Kaista kompiuteris">
         </div>
         <h3 class="text-2xl md:text-3xl font-bold uppercase mb-4">Konsultacija</h3>
         <p class="text-lg md:text-xl">Analizuojame Jūsų poreikius. Nustatome biudžetą. Pateikiame pavyzdžių.</p>
@@ -25,7 +25,7 @@
     <div class="info-block block bg-gray-100 text-center rounded-xl border-solid border-2 border-gray-300 px-3 py-14 w-full">
           <div class="info-block__inside">
        <div class="bg-yellow rounded-full inline-flex w-[131px] h-[131px] text-center p-4 mb-6">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/search-icon.png" width="59" height="62" class="self-center mx-auto">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/search-icon.png" width="59" height="62" class="self-center mx-auto" alt="Lėtas kompiuteris">
         </div>
         <h3 class="text-2xl md:text-3xl font-bold uppercase mb-4">Paieška</h3>
         <p class="text-lg md:text-xl">Operatyviai ieškome tik kokybiškų ir Jūsų biudžetą atitinkančių komponentų.</p>
@@ -35,17 +35,17 @@
     <div class="info-block block text-center rounded-xl border-solid border-2 border-gray-300 px-3 py-14 w-full">
       <div class="info-block__inside">
        <div class="bg-blue rounded-full inline-flex w-[131px] h-[131px] text-center p-4 mb-6">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/puzzle-icon.png" width="59" height="62" class="self-center mx-auto">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/puzzle-icon.png" width="59" height="62" class="self-center mx-auto" alt="Kaista kompiuteris">
         </div>
         <h3 class="text-2xl md:text-3xl font-bold uppercase mb-4">Surinkimas</h3>
         <p class="text-lg md:text-xl">Kruopščiai ir tvarkingai surenkame. Ištestuojame. Suteikiame garantiją dalims bei darbams.</p>
       </div>
     </div>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/right-icon.png" width="29" height="29" class="right-icon self-center rotate-90 xl:rotate-0">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/right-icon.png" width="29" height="29" class="right-icon self-center rotate-90 xl:rotate-0" alt="Kompiuterių taisykla">
     <div class="info-block block bg-gray-100 text-center rounded-xl border-solid border-2 border-gray-300 px-3 py-14 w-full">
       <div class="info-block__inside">
        <div class="bg-yellow rounded-full inline-flex w-[131px] h-[131px] text-center p-4 mb-6">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/delivery-icon.png" width="59" height="62" class="self-center mx-auto">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/delivery-icon.png" width="59" height="62" class="self-center mx-auto" alt="Kompiuterių remontas">
         </div>
         <h3 class="text-2xl md:text-3xl font-bold uppercase mb-4">Pristatymas</h3>
         <p class="text-lg md:text-xl">Kokybiškai supakuojame ir pristatome į kliento namus. Yra galimybė atsiimti salone.</p>
@@ -56,7 +56,7 @@
 
 <div class="flex flex-col lg:flex-row mb-14 md:mb-0">
   <div class="flex-1">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/new-entries.png" class="mx-auto lg:mr-auto lg:ml-0 w-10/12 lg:w-full">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/new-entries.png" class="mx-auto lg:mr-auto lg:ml-0 w-10/12 lg:w-full" alt="Neįsijungia kompiuteris">
   </div>  
   <div class="lg:w-6/12 self-center">
     <h2 class="text-2xl md:text-4xl font-bold uppercase text-center lg:text-left mb-1 md:mb-4">Pradėkime jau dabar!</h2>
