8128252e90a56d364add5607ea767d76c663d4e6
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-10-25 08:59:06 +0300

    Update dirbame-su.php

--- dirbame-su.php
+++ dirbame-su.php
@@ -1,12 +1,12 @@
 <div class="hidden md:block text-center my-14">
   <h2 class="text-2xl md:text-4xl font-extrabold mb-6 uppercase">Dirbame su</h2>
   <div class="working-with-container flex flex-col flex-wrap md:flex-row md:space-x-14 justify-center items-center bg-slate-100 rounded-lg px-4 pt-9 pb-5">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/dell-logo.png" width="80" height="95" class="mb-8 md:mb-4"/>
-    <img src="<?php echo get_template_directory_uri(); ?>/img/lenovo-logo.png" width="190" height="95" class="mb-8 md:mb-4" />
-    <img src="<?php echo get_template_directory_uri(); ?>/img/asus-logo.png" width="150" height="95" class="mb-8 md:mb-4" />
-    <img src="<?php echo get_template_directory_uri(); ?>/img/hp-logo.png" width="80" height="95" class="mb-8 md:mb-4" />
-    <img src="<?php echo get_template_directory_uri(); ?>/img/acer-logo.png" width="120" height="95" class="mb-8 md:mb-4" />
-    <img src="<?php echo get_template_directory_uri(); ?>/img/msi-logo.png" width="125" height="95" class="mb-8 md:mb-4" />
-    <img src="<?php echo get_template_directory_uri(); ?>/img/samsung-logo.png" width="180" height="95" class="mb-8 md:mb-4" />
+    <img src="<?php echo get_template_directory_uri(); ?>/img/dell-logo.png" width="80" height="95" class="mb-8 md:mb-4" alt="Kompiuterių taisykla Vilniuje"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/lenovo-logo.png" width="190" height="95" class="mb-8 md:mb-4" alt="Stacionarių kompiuterių remontas Vilniuje"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/asus-logo.png" width="150" height="95" class="mb-8 md:mb-4" alt="Nešiojamų kompiuterių remontas Vilniuje"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/hp-logo.png" width="80" height="95" class="mb-8 md:mb-4" alt="Kompiuterių remontas Vilniuje"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/acer-logo.png" width="120" height="95" class="mb-8 md:mb-4" alt="Nešiojamų kompiuterių taisykla Vilniuje"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/msi-logo.png" width="125" height="95" class="mb-8 md:mb-4" alt="Kompiuterių remontas Vilniuje"/>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/samsung-logo.png" width="180" height="95" class="mb-8 md:mb-4" alt="Kompiuterių taisykla Vilniuje"/>
   </div>
 </div>
