e286ff9a5ee911dc79afe4bd204c739164222941
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-11-03 19:46:39 +0200

    Update page-supirkimas.php

--- page-supirkimas.php
+++ page-supirkimas.php
@@ -2,7 +2,7 @@
 
 <div class="flex flex-col-reverse lg:flex-row mb-14 md:mb-0">
   <div class="lg:w-5/12 self-center text-center lg:text-left">
-    <h1 class="text-3xl md:text-5xl font-bold uppercase mb-1 md:mb-4">Turite nereikalingą kompiuterį?<br/>Galime jį nupirkti</h1>
+    <h1 class="text-3xl md:text-5xl font-bold mb-1 md:mb-4">Turite nereikalingą kompiuterį?<br/>Galime jį nupirkti</h1>
     <p class="text-lg md:text-2xl text-justify lg:text-left my-2 md:my-5">Užpildykite žemiau esančia formą ir sulaukite atsakymo greičiau nei per 24 valandas! Atlikus išsamią apžiūrą galėsime tiksliai nustatyti įrenginio vertę ir pasiūlyti geriausią kainą.</p>
   </div>
   <div class="flex-1 mb-1 lg:mb-0">
@@ -17,7 +17,7 @@
         <div class="btn btn--blue btn--no-hover rounded-md inline-flex text-center p-6 mb-6">
           <img src="<?php echo get_template_directory_uri(); ?>/img/calendar-icon.png" width="85" height="85" class="self-center mx-auto" alt="Nešiojamų kompiuterių supirkimas Vilniuje">
         </div>
-        <h3 class="text-2xl md:text-3xl font-bold uppercase mb-4">Kompiuterių amžius</h3>
+        <h3 class="text-2xl md:text-3xl font-bold mb-4">Kompiuterių amžius</h3>
         <p class="text-lg md:text-xl">Superkame iki 7 metų senumo nešiojamus ir stacionarius kompiuterius</p>
       </div>  
     </div>
@@ -26,7 +26,7 @@
         <div class="btn btn--yellow btn--no-hover rounded-md inline-flex text-center p-6 mb-6">
           <img src="<?php echo get_template_directory_uri(); ?>/img/check-icon.png" width="95" height="95" class="self-center mx-auto" alt="Nešiojamų kompiuterių taisykla Vilniuje">
         </div>
-        <h3 class="text-2xl md:text-3xl font-bold uppercase mb-4">Modeliai</h3>
+        <h3 class="text-2xl md:text-3xl font-bold mb-4">Modeliai</h3>
         <p class="text-lg md:text-xl">Domina Hp, Dell, Lenovo, Asus, Acer, Msi, Samsung nešiojami ir stacionarūs kompiuteriai</p>
       </div>  
     </div>
@@ -35,7 +35,7 @@
         <div class="btn btn--red btn--no-hover rounded-md inline-flex text-center p-6 mb-6">
           <img src="<?php echo get_template_directory_uri(); ?>/img/warning-icon.png" width="95" height="95" class="self-center mx-auto" alt="Stacionarių kompiuterių remontas Vilniuje">
         </div>
-        <h3 class="text-2xl md:text-3xl font-bold uppercase mb-4">Nesvarbi būklė</h3>
+        <h3 class="text-2xl md:text-3xl font-bold mb-4">Nesvarbi būklė</h3>
         <p class="text-lg md:text-xl">Taip pat superkame neįsijungiančius, sulūžusius, apipiltus nešiojamus ir stacionarius kompiuterius</p>
       </div>
     </div>
@@ -47,7 +47,7 @@
     <img src="<?php echo get_template_directory_uri(); ?>/img/working-desk.png" class="mx-auto lg:mr-auto lg:ml-0 w-10/12" alt="Stacionarių kompiuterių remontas">
   </div>  
   <div class="lg:w-5/12 self-center">
-    <h2 class="text-2xl md:text-4xl font-bold uppercase text-center lg:text-left mb-1 md:mb-4">Supirkimo forma</h2>
+    <h2 class="text-2xl md:text-4xl font-bold text-center lg:text-left mb-1 md:mb-4">Supirkimo forma</h2>
     <?php echo do_shortcode('[contact-form-7 id="581349b" title="Supirkimo forma"]'); ?>
     <!-- DO NOT REMOVE THESE COMMENTS AS THEY ARE HERE TO MAKE SURE TAILWIND FINDS CLASSES USED IN CONTACT FORM 7 FORMS TO COMPILE THEM TO CSS, OTHERWISE THEY WILL NOT BE INCLUDED IN FINAL CSS FILE -->
     <!-- mt-1 cursor-pointer my-1 -->
