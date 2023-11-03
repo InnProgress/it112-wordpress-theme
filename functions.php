68bf9e1a727522d0c83440ac1d0d6d3864cf2cfa
Arnold Autuch <arnoldautuch4@gmail.com>
2023-11-03 19:11:29 +0200

    update

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_theme_support('menus');
 add_theme_support('post-thumbnails');
 
 function loadThemeStylesAScripts() {
-  wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css2?family=Asap:wght@400;600;700;800');
+  wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css2?family=Gabarito:wght@400;600;700;800');
   wp_enqueue_style('style', get_template_directory_uri() . '/css/output.css');
   wp_enqueue_script('script', get_template_directory_uri() . '/script.js');
 }
--- page-kontaktai.php
+++ page-kontaktai.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <div class="flex flex-col-reverse md:flex-row mb-14 mt-6 lg:mb-0">
-  <div class="flex-1 self-center">
-    <div class="lg:pl-[80px]">
+  <div class="flex flex-1 self-center justify-center">
+    <div class="w-max">
       <h2 class="text-3xl md:text-4xl font-bold mb-4 md:mb-7">MŪSŲ KONTAKTAI</h2>
       <div class="flex items-center">
         <div class="w-10 md:w-20">
@@ -25,7 +25,7 @@
     </div>
   </div>
   <div class="flex-1 mb-4 md:mb-0">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/get-in-touch.png" class="mx-auto md:mx-0 w-10/12" alt="Kompiuterių taisykla Vilniuje">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/get-in-touch.png" class="mx-auto lg:mr-0 w-10/12" alt="Kompiuterių taisykla Vilniuje">
   </div>
 </div>
 
@@ -35,21 +35,23 @@
   </div>
   <div class="flex-1 self-center text-center md:text-left">
     <h2 class="text-2xl md:text-4xl font-bold uppercase mb-1 md:mb-4">Itin patogus susisiekimas!</h2>
-    <p class="text-lg md:text-xl text-justify md:text-left xl:w-10/12 my-3 md:my-5">Atvykite į mūsų saloną autobusu ar troleibusu. Visai šalia mūsų yra viena populiariausių stotelių „Žaliasis tiltas“. Čia važiuoja autobusai: 1G, 3G, 6G, 10, 43, 46, 52, 53, 56, 88 bei troleibusai: 6, 10, 12.</p>
+    <p class="text-lg md:text-xl text-justify md:text-left my-3 md:my-5">Atvykite į mūsų saloną autobusu ar troleibusu. Visai šalia mūsų yra viena populiariausių stotelių „Žaliasis tiltas“. Čia važiuoja autobusai: 1G, 3G, 6G, 10, 43, 46, 52, 53, 56, 88 bei troleibusai: 6, 10, 12.</p>
     <a href="https://www.stops.lt/vilnius/#stop/0101,0102" class="btn btn--red rounded-lg inline-block text-md md:text-xl px-14 py-5">TVARKARAŠTIS</a>
   </div>
 </div>
 
 <div class="flex flex-col-reverse lg:flex-row mb-14 md:mb-0"> 
   <div class="lg:w-6/12 self-center">
-    <h2 class="text-2xl md:text-4xl font-bold uppercase text-center lg:text-left mb-1 md:mb-4">Kreipkitės jau dabar!</h2>
-    <p class="text-lg md:text-2xl text-center lg:text-left mt-2 mb-3 md:mt-4 md:mb-6">Visada stengiamės atsakyti kaip įmanoma greičiau.</p>
-    
-    <?php echo do_shortcode('[contact-form-7 id="47162d4" title="Kontaktų forma"]'); ?>
-    <!-- DO NOT REMOVE THESE COMMENTS AS THEY ARE HERE TO MAKE SURE TAILWIND FINDS CLASSES USED IN CONTACT FORM 7 FORMS TO COMPILE THEM TO CSS, OTHERWISE THEY WILL NOT BE INCLUDED IN FINAL CSS FILE -->
-    <!-- mt-1 mt-6 cursor-pointer -->
+    <div class="lg:w-10/12">
+      <h2 class="text-2xl md:text-4xl font-bold uppercase text-center lg:text-left mb-1 md:mb-4">Kreipkitės jau dabar!</h2>
+      <p class="text-lg md:text-2xl text-center lg:text-left mt-2 mb-3 md:mt-4 md:mb-6">Visada stengiamės atsakyti kaip įmanoma greičiau.</p>
+      
+      <?php echo do_shortcode('[contact-form-7 id="47162d4" title="Kontaktų forma"]'); ?>
+      <!-- DO NOT REMOVE THESE COMMENTS AS THEY ARE HERE TO MAKE SURE TAILWIND FINDS CLASSES USED IN CONTACT FORM 7 FORMS TO COMPILE THEM TO CSS, OTHERWISE THEY WILL NOT BE INCLUDED IN FINAL CSS FILE -->
+      <!-- mt-1 mt-6 cursor-pointer -->
+    </div>
   </div>
-    <div class="flex-1">
+  <div class="flex-1">
     <img src="<?php echo get_template_directory_uri(); ?>/img/new-entries.png" class="mx-auto lg:mr-0 w-10/12 lg:w-11/12" alt="Nešiojamų kompiuterių taisykla Vilniuje">
   </div> 
 </div>
