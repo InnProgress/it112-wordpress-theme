636acfe177cfc9e54f22665b4120a8f0fefaafc5
Arnas Bulka <40238781+Arnanasas@users.noreply.github.com>
2023-11-19 18:49:50 +0200

    gtm

--- header.php
+++ header.php
@@ -29,9 +29,21 @@
   gtag('config', 'G-EXFFR5JX4J');
 </script>
 
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PFVBJPJ7');</script>
+<!-- End Google Tag Manager -->
+
   
 </head>
 <body <?php body_class(); ?>>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFVBJPJ7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
   <div class="container mx-auto px-3">
     <header class="relative z-10 flex justify-between bg-white text-black py-5">
       <a href="<?php echo esc_url(home_url( '/' )); ?>" class="flex self-center"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" width="100" alt="Kompiuterių remontas Vilniuje"></a>
