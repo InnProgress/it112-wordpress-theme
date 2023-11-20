7f5ac8c2b7c92889525b722a082cabf45d9c4c8a
Arnas Bulka <40238781+Arnanasas@users.noreply.github.com>
2023-11-20 12:46:28 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -178,6 +178,6 @@
 </div>
 
 <?php require 'dirbame-su.php'; ?>
-<?php echo do_shortcode([wp_schema_pro_rating_shortcode]); ?>
+<?php echo do_shortcode("[wp_schema_pro_rating_shortcode]"); ?>
 
 <?php get_footer(); ?>
