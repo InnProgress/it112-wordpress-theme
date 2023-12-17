d271e2754326006debe2849997c449c247aaa4bd
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-12-17 16:56:23 +0200

    Update kompiuteriai.php

--- template-parts/kompiuteriai.php
+++ template-parts/kompiuteriai.php
@@ -127,6 +127,135 @@
   </div>
 </div>
 
+<div class="my-20">
+  <h2 class="text-2xl md:text-4xl font-bold text-center mb-3 lg:mb-8">Kompiuteriai darbui ir mokslams</h2>
+  <div class="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-6 mb-7">
+    <div class="block flex-1 rounded-2xl">
+      <div class="relative border-2 border-gray-300 rounded-xl h-full py-6">
+<!--         <-- <a href="" class="absolute left-8 top-8 z-10 btn btn--yellow rounded-md xl:text-lg px-8 py-3">Koreguoti</a> -->
+
+        <div class="carousel-wrapper relative">
+          <button class="carousel__prev group absolute z-10 left-8 top-2/4">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid.svg" alt="left" class="rotate-180 group-hover:hidden" />
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid-black.svg" alt="left" class="rotate-180 hidden group-hover:block" />
+          </button>
+          <button class="carousel__next group absolute z-10 right-8 top-2/4">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid.svg" alt="right" class="group-hover:hidden" />
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid-black.svg" alt="right" class="hidden group-hover:block" />
+          </button>
+          
+          <div class="carousel relative h-80 px-7 overflow-hidden">  
+            <img src="<?php echo get_template_directory_uri(); ?>/img/pc1.png" alt="computer" class="carousel__item w-full h-80 object-contain" />
+          </div>
+        </div>
+        <div class="py-4 px-7">
+          <h3 class="text-xl md:text-2xl font-bold uppercase px-2">Žaidimų kompiuteris IT1</h3>
+          <span class="block text-lg text-gray-400 px-2 mt-2">Puikiai suderinti komponentai leis žaisti įvairius žaidimus, sklandžiai dirbti ar mokytis neišleidžiant didelės pinigų sumos.</span>
+          <div class="grid grid-cols-2 bg-neutral-100 text-custom-gray font-medium rounded-md my-4 px-2 py-2">
+            <p>Procesorius</p>
+            <p>Ryzen 5 5500</p>
+            
+            <p>Vaizdo plokštė</p>
+            <p>RX 6600 8GB</p>
+            
+            <p>RAM</p>
+            <p>8GB 3200MHz</p>
+
+            <p>Diskas</p>
+            <p>512GB-2TB M.2 SSD</p>
+          </div>
+          <div class="flex justify-between items-center px-2">
+            <p class="text-2xl xl:text-4xl font-bold">569€</p>
+            <a href="https://it112.lt/produktas/zaidimu-kompiuteris-it1-ryzen-5-5500-rx-6600-8gb-ram-512gb-ssd-m-2/" class="btn btn--blue rounded-md xl:text-lg px-16 py-3">Pirkti</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="block flex-1 rounded-2xl">
+      <div class="relative border-2 border-gray-300 rounded-xl h-full py-6">
+<!--         <-- <a href="" class="absolute left-8 top-8 z-10 btn btn--yellow rounded-md xl:text-lg px-8 py-3">Koreguoti</a> -->
+
+        <div class="carousel-wrapper relative">
+          <button class="carousel__prev group absolute z-10 left-8 top-2/4">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid.svg" alt="left" class="rotate-180 group-hover:hidden" />
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid-black.svg" alt="left" class="rotate-180 hidden group-hover:block" />
+          </button>
+          <button class="carousel__next group absolute z-10 right-8 top-2/4">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid.svg" alt="right" class="group-hover:hidden" />
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid-black.svg" alt="right" class="hidden group-hover:block" />
+          </button>
+          
+          <div class="carousel relative h-80 px-7 overflow-hidden">  
+            <img src="<?php echo get_template_directory_uri(); ?>/img/pc.png" alt="computer" class="carousel__item w-full h-80 object-contain" />
+          </div>
+        </div>
+        <div class="py-4 px-7">
+          <h3 class="text-xl md:text-2xl font-bold uppercase px-2">Žaidimų kompiuteris IT2</h3>
+          <span class="block text-lg text-gray-400 px-2 mt-2">Galingas ir išvaizdus kompiuteris, susitvarkys su visais žaidimais vidutinėmis/maksimaliomis grafikomis.</span>
+          <div class="grid grid-cols-2 bg-neutral-100 text-custom-gray font-medium rounded-md my-4 px-2 py-2">
+            <p>Procesorius</p>
+            <p>Ryzen 5 5600X</p>
+            
+            <p>Vaizdo plokštė</p>
+            <p>RTX 4060 8GB</p>
+            
+            <p>RAM</p>
+            <p>16GB 3200MHz</p>
+
+            <p>Diskas</p>
+            <p>512GB-2TB M.2 SSD</p>
+          </div>
+          <div class="flex justify-between items-center px-2">
+            <p class="text-2xl xl:text-4xl font-bold">999€</p>
+            <a href="https://it112.lt/produktas/zaidimu-kompiuteris-midi-ryzen-5-5600x-rtx-4060-16gb-ram-512gb-ssd-m-2/" class="btn btn--blue rounded-md xl:text-lg px-16 py-3">Pirkti</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="block flex-1 rounded-2xl">
+      <div class="relative border-2 border-gray-300 rounded-xl h-full py-6">
+<!--         <a href="" class="absolute left-8 top-8 z-10 btn btn--yellow rounded-md xl:text-lg px-8 py-3">Koreguoti</a> -->
+
+        <div class="carousel-wrapper relative">
+          <button class="carousel__prev group absolute z-10 left-8 top-2/4">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid.svg" alt="left" class="rotate-180 group-hover:hidden" />
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid-black.svg" alt="left" class="rotate-180 hidden group-hover:block" />
+          </button>
+          <button class="carousel__next group absolute z-10 right-8 top-2/4">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid.svg" alt="right" class="group-hover:hidden" />
+            <img src="<?php echo get_template_directory_uri(); ?>/img/chevron-right-solid-black.svg" alt="right" class="hidden group-hover:block" />
+          </button>
+          
+          <div class="carousel relative h-80 px-7 overflow-hidden">  
+            <img src="<?php echo get_template_directory_uri(); ?>/img/pc.png" alt="computer" class="carousel__item w-full h-80 object-contain" />
+          </div>
+        </div>
+        <div class="py-4 px-7">
+          <h3 class="text-xl md:text-2xl font-bold uppercase px-2">Žaidimų kompiuteris IT3</h3>
+          <span class="block text-lg text-gray-400 px-2 mt-2">Itin galingas ir išvaizdus kompiuteris, susitvarkys su visais žaidimais be jokio vargo. Vandeniu aušinamas procesorius užtikrins sklandų ir greitą darbą.</span>
+          <div class="grid grid-cols-2 bg-neutral-100 text-custom-gray font-medium rounded-md my-4 px-2 py-2">
+            <p>Procesorius</p>
+            <p>Ryzen 9 5900X</p>
+            
+            <p>Vaizdo plokštė</p>
+            <p>RTX 4070 12GB</p>
+            
+            <p>RAM</p>
+            <p>32GB</p>
+
+            <p>Diskas</p>
+            <p>512GB-2TB M.2 SSD</p>
+          </div>
+          <div class="flex justify-between items-center px-2">
+            <p class="text-2xl xl:text-4xl font-bold">1529€</p>
+            <a href="https://it112.lt/produktas/zaidimu-kompiuteris-maxi-ryzen-9-5900x-rtx-4070-32gb-ram-1tb-ssd-m-2/" class="btn btn--blue rounded-md xl:text-lg px-16 py-3">Pirkti</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script>
   const carousels = document.querySelectorAll(".carousel-wrapper");
   carousels.forEach(carouselWrapper => {
