6393570a26f8d07386a211e9cf191d90e5eaebff
JustinasAug <91558055+intenseCeon@users.noreply.github.com>
2023-12-17 17:07:24 +0200

    Update kompiuteriai.php

--- template-parts/kompiuteriai.php
+++ template-parts/kompiuteriai.php
@@ -153,10 +153,10 @@
           <span class="block text-lg text-gray-400 px-2 mt-2">Kokybiški komponentai leis dirbti sklandžiai ir greitai.</span>
           <div class="grid grid-cols-2 bg-neutral-100 text-custom-gray font-medium rounded-md my-4 px-2 py-2">
             <p>Procesorius</p>
-            <p>Ryzen 5 5500</p>
+            <p>Ryzen 5 5600G</p>
             
             <p>Vaizdo plokštė</p>
-            <p>RX 6600 8GB</p>
+            <p>Radeon™ Graphics</p>
             
             <p>RAM</p>
             <p>8GB 3200MHz</p>
@@ -165,7 +165,7 @@
             <p>512GB-2TB M.2 SSD</p>
           </div>
           <div class="flex justify-between items-center px-2">
-            <p class="text-2xl xl:text-4xl font-bold">569€</p>
+            <p class="text-2xl xl:text-4xl font-bold">399€</p>
             <a href="https://it112.lt/produktas/zaidimu-kompiuteris-it1-ryzen-5-5500-rx-6600-8gb-ram-512gb-ssd-m-2/" class="btn btn--blue rounded-md xl:text-lg px-16 py-3">Pirkti</a>
           </div>
         </div>
